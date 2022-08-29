19ab8def92de6c438cada5f4a60434e6b1365d84
mhson-stack <72399227+mhson-stack@users.noreply.github.com>
2022-08-29 13:37:09 +0900

    delete template

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,46 +1,54 @@
 import os
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask import Flask, request
+from datetime import datetime
+from dotenv import load_dotenv
+import psycopg2
+import pandas as pd
+
+
+load_dotenv()
+HOST = os.getenv("HOST")
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hello'
-
-socket_io = SocketIO(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    """
-    index 함수에서는 '/' 엔드 포인트로 접속했을 때 'index.html' 파일을
-    렌더링 해줍니다.
-
-    'index.html' 파일에서 'users.csv' 파일에 저장된 유저 목록을 보여줄 수 있도록
-    유저들을 html 파일에 넘길 수 있어야 합니다.
-    """
-
-    return render_template('index.html'), 200
-
-@app.route('/crypto/', defaults={'symbol' : 'BTC'})
-@app.route('/crypto/<symbol>', methods=['GET'])
-def graph(symbol):
-    return render_template('graph.html'), 200
-
-@app.route("/main/")
-def main():
-    return render_template("main.html"), 200
+    return 200
 
 
-@socket_io.on("message")
-def request(message):
-    print("message : "+ message)
-    to_client = dict()
-    if message == 'new_connect':
-        to_client['message'] = "새로운 유저가 난입하였다!!"
-        to_client['type'] = 'connect'
-    else:
-        to_client['message'] = message
-        to_client['type'] = 'normal'
-    send(to_client, broadcast=True)
+@app.route('/crypto/', methods=['GET'])
+def get_price():
+    symbols = ["BTC", "ETH", "ETC", "XRP", "BNB"]
+    symbol = request.args.get('symbol', default=None, type=str)
+    year = request.args.get('year', default=datetime.today().year, type=str)
+    month = request.args.get('month', default=datetime.today().month, type=str)
+    date = request.args.get('date', default=datetime.today().day, type=str)
+    hour = request.args.get('hour', default=None, type=str)
+    
+    if symbol == None:
+      return "No symbol given", 400
+    if symbol not in symbols:
+      return f"Ticker '{symbol}' doesn't exist", 404
+    if hour is None:
+        return "Sepcifiy Open Hour"
 
+    conn_string = f"postgresql://{USER}:{PASSWORD}@{HOST}/{DB_NAME}"
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM \"public\".\"{symbol}\"")
+    df = cur.fetchall()
+    conn.commit()
+    conn.close()
+    cols = ["OpenTime", "Open", "High", "Low", "Close", "Volume", "CloseTime", 
+            "QuoteAssetVolume", "NumTrades", "TakerBuyBaseAssetVolume", 
+            "TakerBuyQuoteAssetVolume", "Ignore"]
+    df = pd.DataFrame(df, columns=cols)
+    dt = datetime(year, month, date, hour, 0)
+    df[df['OpenTime'] == dt]
+    return df["Open"]
 
 
 if __name__ == '__main__':
